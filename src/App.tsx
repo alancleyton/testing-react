17e532718f80c0ee91a4e499ace9a2dc1d4a4e66
Alan Cleyton <alancleyton67@gmail.com>
2023-04-08 10:27:29 -0300

    ESlint and Prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import React from 'react';
+
+function App(): JSX.Element {
   return (
     <div className="App">
       <h2>Hello World!</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const app = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const app = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 app.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
