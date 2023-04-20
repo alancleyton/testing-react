54490c50a7c372bd621a25b0dbf6ceccf7ded0ba
Alan Cleyton <alancleyton67@gmail.com>
2023-04-20 16:49:53 -0300

    Refactor login page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,1 +1,16 @@
-export { Home as default } from './home';
+import React from 'react';
+import ReactIcon from '../../../public/react.svg';
+import './styles.css';
+
+const Home = (): JSX.Element => {
+  return (
+    <main className="home">
+      <header className="home-header">
+        <img className="home-header-icon" src={ReactIcon} alt="React" />
+        <h2 className="home-header-heading">Testing React Apps</h2>
+      </header>
+    </main>
+  );
+};
+
+export default Home;
